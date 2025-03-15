94f2173b6df79478a76462e5159826ad8a7bd6fc
sirsegv <sirsegv@sirsegv.moe>
2025-03-15 13:08:39 +1030

    new whiteboard and unify websocket code

--- interview_platform/core/consumers.py
+++ interview_platform/core/consumers.py
@@ -1,50 +1,35 @@
-#This allows real-time text updates in the shared editor!
-#websocket logic
+# This allows real-time text updates in the shared editor!
+# websocket logic
+
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class EditorConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.channel_layer.group_add("editor_room", self.channel_name)
-        await self.accept()
+class WSConsumer(AsyncWebsocketConsumer):
+	async def connect(self):
+		await self.channel_layer.group_add("room", self.channel_name)
+		await self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard("editor_room", self.channel_name)
+	async def disconnect(self, close_code):
+		await self.channel_layer.group_discard("room", self.channel_name)
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        await self.channel_layer.group_send(
-            "editor_room",
-            {
-                "type": "editor_update",
-                "content": data["content"]
-            }
-        )
+	async def receive(self, text_data):
+		data = json.loads(text_data)
+		await self.channel_layer.group_send(
+			"room",
+			{
+				"type": data["type"], # forward updates to text or whiteboard based on received info
+				"data": data["data"]
+			}
+		)
 
-    async def editor_update(self, event):
-        await self.send(text_data=json.dumps({
-            "content": event["content"]
-        }))
-        
-class WhiteboardConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.channel_layer.group_add("whiteboard_room", self.channel_name)
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard("whiteboard_room", self.channel_name)
-
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        await self.channel_layer.group_send(
-            "whiteboard_room",
-            {
-                "type": "whiteboard_update",
-                "draw_data": data["draw_data"]
-            }
-        )
-
-    async def whiteboard_update(self, event):
-        await self.send(text_data=json.dumps({
-            "draw_data": event["draw_data"]
-        }))
+	async def txt_update(self, event):
+		await self.send(text_data=json.dumps({
+			"type": "txt_update",
+			"data": event["data"]
+		}))
+	
+	async def wb_buffer(self, event):
+		await self.send(text_data=json.dumps({
+			"type": "wb_buffer",
+			"data": event["data"]
+		}))
