eb2a9ec456e8f1a4cb3833a4071f65a396bfabe0
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2025-03-16 16:03:14 +1100

    Fix migration and deployment scripts

--- interview_platform/deploy.sh
+++ interview_platform/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "Starting deployment script..."
 echo "Running migrations..."
-python migrate.py
+python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 echo "Starting Daphne server..."
-daphne interview_platform.asgi:application --port $PORT --bind 0.0.0.0
-python manage.py runserver 0.0.0.0:$PORT 
+daphne interview_platform.asgi:application --port $PORT --bind 0.0.0.0 
